b181fcbd738418e870017ce387f779d1da61f621
Muhammed Aflah <31723966+aflahkp@users.noreply.github.com>
2022-01-30 20:06:18 +0530

    Time: 254 ms (5.32%), Space: 50.7 MB (5.67%) - LeetHub

---
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.js
@@ -0,0 +1,39 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var removeOuterParentheses = function(s) {
+  if(!s){
+      return true;
+  }
+    const stack = [];
+    var temp = [];
+    var result = '';
+    
+    for(let i=0;i<s.length;i++){
+        temp.push(s[i]);
+        if(s[i] == "("){
+         stack.push(i) 
+        }
+        else{
+           if(stack.length == 1){
+            if(stack.pop() < i-1){
+                temp = temp.slice(1,temp.length-1)
+                 console.log(temp)
+                while(temp.length){
+                    result+=temp.shift()
+                }
+            }
+               else{
+                   temp = [];
+               }
+                   
+           }
+            else{
+                stack.pop()    
+            }
+        }
+
+    }
+    return result
+};
