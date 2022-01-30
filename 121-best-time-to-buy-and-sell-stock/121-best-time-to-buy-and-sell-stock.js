f9681cd07de4eeada27b265d0cfe5b7d58d0bbd5
Muhammed Aflah <31723966+aflahkp@users.noreply.github.com>
2022-01-30 12:50:12 +0530

    Time: 181 ms (11.57%), Space: 52.2 MB (5.12%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.js
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.js
@@ -3,21 +3,13 @@
  * @return {number}
  */
 var maxProfit = function(prices) {
-    var maxProfit = Number.NEGATIVE_INFINITY;
-    const len = prices.length;
-    var i = 1;
-    var prev = prices[0];
+    var maxProfit = 0;
+    var min = Number.POSITIVE_INFINITY;
     
-    
-    while(i<len){
-        const current = prices[i];
-        if(current -prev >maxProfit){
-            maxProfit = current-prev;
-        }
-        if(prev>current){
-            prev = current;
-        }
-        i++;
+    for(let price of prices){
+        min = Math.min(min,price);
+        maxProfit = Math.max(maxProfit, price-min)
     }
-    return maxProfit>0?maxProfit:0;
+    
+    return maxProfit;
 };
