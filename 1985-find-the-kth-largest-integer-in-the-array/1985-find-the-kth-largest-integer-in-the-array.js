f9a74b232e8a5f156ed355a653fa77fdece5ee30
Muhammed Aflah <31723966+aflahkp@users.noreply.github.com>
2022-02-22 09:02:56 +0530

    Time: 568 ms (21.95%), Space: 61.9 MB (17.07%) - LeetHub

--- 1985-find-the-kth-largest-integer-in-the-array/1985-find-the-kth-largest-integer-in-the-array.js
+++ 1985-find-the-kth-largest-integer-in-the-array/1985-find-the-kth-largest-integer-in-the-array.js
@@ -21,13 +21,38 @@ var kthLargestNumber = function(nums, k) {
 var MaxHeap = function(){
     let heap = [null];
     
+    
+    
+    var compareLE = (a,b) => {
+        if(!b){
+            return false;
+        }
+        if(a?.length<b?.length){
+            return true;
+        }
+        else{
+            return BigInt(a)<=BigInt(b);
+        }
+    }
+    
+    var compareG = (a,b) => {
+        if(!b){
+            return true;
+        }
+         if(a?.length>b?.length){
+            return true;
+        }
+        else{
+            return BigInt(a)>BigInt(b);
+        }
+    }
+    
     this.add = function(n){
-        let num = BigInt(n);
-        heap.push(num);
+        heap.push(n);
         
         if(heap.length>2){
             let idx = heap.length-1;
-            while(heap[idx]>heap[Math.floor(idx/2)]){
+            while(compareG(heap[idx],heap[Math.floor(idx/2)])){
                 [heap[idx],heap[Math.floor(idx/2)]] = [heap[Math.floor(idx/2)],heap[idx]];
                 
                 if(Math.floor(idx/2) >1){
@@ -51,8 +76,8 @@ var MaxHeap = function(){
         let result = heap[1];
         heap[1] = (heap.pop());
         let idx = 1;
-        while(heap[idx]<=heap[2*idx]|| heap[idx]<=heap[2*idx+1]){
-            if(heap[2*idx]>heap[2*idx+1] || !heap[2*idx+1]){
+        while(compareLE(heap[idx],heap[2*idx])|| compareLE(heap[idx],heap[2*idx+1])){
+            if(compareG(heap[2*idx],heap[2*idx+1]) || !heap[2*idx+1]){
                 [heap[idx],heap[2*idx]] = [heap[2*idx],heap[idx]];
                 idx = 2*idx;
             }
