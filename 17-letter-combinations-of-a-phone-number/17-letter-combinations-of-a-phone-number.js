ff504f064b24f711c08f26ef7549ea83cad7a1c5
Muhammed Aflah <31723966+aflahkp@users.noreply.github.com>
2022-02-28 20:31:46 +0530

    Time: 67 ms (84.16%), Space: 42.2 MB (22.38%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.js
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.js
@@ -21,18 +21,18 @@ var letterCombinations = function(digits) {
     var prepareComb = (acc, curr) => {
         var digit = digits[curr];
         for(let c of keys[digit]){
-            acc.push(c);
+            acc+=c;
             if(acc.length == digits.length){
-                combs.push(acc.join(""))
+                combs.push(acc)
             }
             else{
              prepareComb(acc,curr+1)   
             }
-            acc.pop();
+            acc = acc.slice(0,-1)
         }
     }
     
-    prepareComb([],0)
+    prepareComb("",0)
     return combs;
     
     
