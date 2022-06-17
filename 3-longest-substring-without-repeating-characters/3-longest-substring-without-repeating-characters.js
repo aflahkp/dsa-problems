dbb5ad576cafca23dbebabd605e61fbebcee4e91
Muhammed Aflah <31723966+aflahkp@users.noreply.github.com>
2022-06-17 08:17:42 +0530

    Time: 128 ms (61.95%), Space: 44.7 MB (93.82%) - LeetHub

---
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.js
@@ -0,0 +1,30 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function(s) {
+    if(s.length==0) return 0;
+    let n= s.length
+    let maxLen = 1;
+    let map = new Map();
+    map.set(s[0],0)
+    
+    let l = 0;
+    let r = 1;
+    
+    while(r<n){
+        let char = s[r];
+        if(map.has(char)){
+            l=Math.max(l,map.get(char)+1);
+            // console.log(l,r,map,char)
+        }
+        maxLen = Math.max(maxLen,r-l+1)
+        // console.log(l,r,maxLen,map)
+        map.set(char,r)
+        r++;
+        
+    }
+    
+    return maxLen;
+    
+};
